5206603a59eb77e01eca75a547efd07085b7f0e3
turner-prize <hiphoperation@hotmail.com>
2018-10-22 15:17:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import logging
+
+def f():
+    return g()
+
+def g():
+    return h()
+
+def h():
+    logging.debug('About to do h().')
+    return i()
+
+def i():
+    1/0
+
+def e():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='kaboom1.log',
+        filemode='w')
+
+    logging.debug('About to do f().')
+
+    try:
+        f()
+
+    except Exception as ex:
+        logging.exception(ex)
+        raise
+
+
+if __name__ == '__main__':
+    e()
